35c48b9847d00998773aee0747834bb946030aa1
Daniel Andrew <dwandrew@hotmail.com>
2020-03-01 14:59:43 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
@@ -12,7 +14,7 @@ def crew
 end
 
 def greet_crew (crew)
-  crew.each {|crew_member| puts "Hello #{crew_member}."}
+  crew.each {|crew_member| "Hello #{crew_member}."} binding.pry
 end
 
 def engage
